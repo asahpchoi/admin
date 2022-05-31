ce1c3d6ea7efb295b017c75c22a5f86b9ad4be76
Asa Choi <asa.choi@gmail.com>
2022-05-31 08:13:43 +0000

    X

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "core-js";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+//import * as serviceWorker from './worker';
+
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -18,3 +20,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//serviceWorker.register();
